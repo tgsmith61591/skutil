eca80aa5eee343e2d59aff8f1bf056a558b33d85
tgsmith61591 <tgsmith61591@gmail.com>
2016-05-24 20:19:52 -0500

    Added another fix

--- skutil/__init__.py
+++ skutil/__init__.py
@@ -4,7 +4,7 @@ sklearn-esque transformers for python
 import sys, warnings
 from .base import *
 
-__version__ = '0.0.7'
+__version__ = '0.0.8'
 
 try:
     # This variable is injected in the __builtins__ by the build
