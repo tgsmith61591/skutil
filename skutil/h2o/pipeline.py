f6a0dad1996e1c1692bc9e63e1a23c8afd1d3532
Taylor Smith <tgsmith61591@gmail.com>
2016-08-10 12:17:47 -0500

    Better error logging in h2o pipeline

--- skutil/h2o/pipeline.py
+++ skutil/h2o/pipeline.py
@@ -138,7 +138,8 @@ class H2OPipeline(BaseH2OFunctionWrapper):
 		
 		# we need a y for the pipeline
 		if not isinstance(y, str):
-			raise TypeError('target_feature should be a single string')
+			raise TypeError('target_feature should be a single string. '
+							'Got %s (type=%s)' % (str(y), type(y)))
 		
 		# make sure they're strings:
 		for n in (x): # in a tuple in case we add another self param with names
