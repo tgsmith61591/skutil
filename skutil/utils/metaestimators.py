c6825cad0db513d763c9429a88ee692a3b6a2604
Taylor Smith <tgsmith61591@gmail.com>
2016-10-07 09:50:01 -0500

    Adapted meta-estimator script for use with sklearn 0.17

--- skutil/utils/metaestimators.py
+++ skutil/utils/metaestimators.py
@@ -1,10 +1,63 @@
+"""Utilities for meta-estimators"""
+# Author: Joel Nothman
+#         Andreas Mueller
+# Adapted by Taylor Smith for use with skutil
+# License: BSD
+
 from __future__ import print_function, absolute_import, division
-from sklearn.utils.metaestimators import _IffHasAttrDescriptor
+from functools import update_wrapper
+from operator import attrgetter
 
 
 __all__ = [
     'if_delegate_has_method'
 ]
+
+
+class _IffHasAttrDescriptor(object):
+    """Implements a conditional property using the descriptor protocol.
+    Using this class to create a decorator will raise an ``AttributeError``
+    if none of the delegates (specified in ``delegate_names``) is an attribute
+    of the base object or the first found delegate does not have an attribute
+    ``attribute_name``. THIS IS ADAPTED FROM SKLEARN 0.18, AS IT ADDS NEW
+    FUNCTIONALITY AND ALLOWS US TO SEARCH FOR A SPECIFIC ATTRIBUTE NAME.
+
+    This allows ducktyping of the decorated method based on
+    ``delegate.attribute_name``. Here ``delegate`` is the first item in
+    ``delegate_names`` for which ``hasattr(object, delegate) is True``.
+
+    See https://docs.python.org/3/howto/descriptor.html for an explanation of
+    descriptors.
+    """
+    def __init__(self, fn, delegate_names, attribute_name):
+        self.fn = fn
+        self.delegate_names = delegate_names
+        self.attribute_name = attribute_name
+
+        # update the docstring of the descriptor
+        update_wrapper(self, fn)
+
+    def __get__(self, obj, type=None):
+        # raise an AttributeError if the attribute is not present on the object
+        if obj is not None:
+            # delegate only on instances, not the classes.
+            # this is to allow access to the docstrings.
+            for delegate_name in self.delegate_names:
+                try:
+                    delegate = attrgetter(delegate_name)(obj)
+                except AttributeError:
+                    continue
+                else:
+                    getattr(delegate, self.attribute_name)
+                    break
+            else:
+                attrgetter(self.delegate_names[-1])(obj)
+
+        # lambda, but not partial, allows help() to work with update_wrapper
+        out = lambda *args, **kwargs: self.fn(obj, *args, **kwargs)
+        # update the docstring of the returned function
+        update_wrapper(out, self.fn)
+        return out
 
 def if_delegate_has_method(delegate, method=None):
     """Create a decorator for methods that are delegated to a sub-estimator
