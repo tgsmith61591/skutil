d7466c79ac687c36791da0780e2602a14d751af4
charlesdrotar <drotarcharles@gmail.com>
2016-10-23 23:35:39 -0500

    Update publish_gh_pages.sh
    Fixing the bug in shell script that does not allow gh-pages to properly update the version.

--- doc/publish_gh_pages.sh
+++ doc/publish_gh_pages.sh
@@ -1,53 +1,85 @@
-###########################################
-##This script should be run immediately####
-##after a push has been made to the master#
-##branch to update the gh-pages branch#####
-##You will want to run this script from####
-##its current location like as follows:####
-##sh publish_gh_pages.sh "commit message"##
-###########################################
+#!/usr/bin/env bash
 
-# This script will check if the number of input arguments are 1
-#   A) If arguments == 1
-#       1) make clean html                  # purge old docs and create new
-#       2) git stash                        # delete unwanted/uncommited changes to master
-#       3) git checkout gh-pages            # switch to gh-pages
-#       4) cp -R ../gh-pages/html/* ../     # copy new html to base directory of gh-pages
-#       5) git add -A                       # add the new and modified files
-#       6) git commit -m "$COMMIT_MESSAGE"  # commit the added changes
-#       7) git push origin gh-pages         # push the changes to gh-pages
-#       8) git checkout master              # return to master
-#   B) If arguments != 1
+#########
+# NOTES #
+#########
+
+# This script should be run immediately after a push
+#   has been made to the master branch to update the gh-pages branch.
+
+# As part of those changes we need to rebuild the project
+#   to pull in the new updated version number.
+
+# You will want to run this script from
+#   its current location (i.e. sh publish_gh_pages.sh)
+
+#########
+# STEPS #
+#########
+
+# This script will check if the number of input arguments are 0
+#   A) If arguments == 0
+#       0) python setup.py develop           # make sure that package is rebuilt to pull in latest versions number
+#       1) make clean html                   # purge old docs and create new
+#       2) cp -R ../ghpages/html ../         # copy html folder to base directory of master
+#       3) git stash                         # delete unwanted/uncommited changes to master
+#       4) git checkout gh-pages             # switch to gh-pages
+#       5) git pull origin gh-pages          # pull in latest changes from gh-pages
+#       6) cp -R ../html/* ..                # copy new html to base directory of gh-pages
+#       7) rm -r ../html                     # remove html folder
+#       8) git add -A                        # add the new and modified files
+#       9) git commit -m "$COMMIT_MESSAGE"   # commit the added changes
+#       10) git push origin gh-pages         # push the changes to gh-pages
+#       11) git checkout master              # return to master
+#   B) If arguments != 0
 #       1) print messages on proper usage
 
+#############
+# EXECUTION #
+#############
+if test $# -eq 0; then
 
-if test $# -eq 1; then
+    VERSION_NUMBER=`cat ../skutil/__init__.py | grep "version" | tr "=" "\n" | grep -v "version"`
+    COMMIT_MESSAGE="Update for version - $VERSION_NUMBER"
+    COMMIT_TIME=`date "+%m/%d/%Y %H:%M"`
 
-    COMMIT_MESSAGE="$1"
+    echo "\n\nSee note A.0) python setup.py develop\n\n"
+    cd ..
+    python setup.py develop
 
     echo "\n\nSee note A.1) make clean html\n\n"
+    cd doc
     make clean html
 
-    echo "\n\nSee note A.2) git stash\n\n"
+    echo "\n\nSee note A.2) ../gh-pages/html ../\n\n"
+    cp -R ../gh-pages/html ../
+
+    echo "\n\nSee note A.3) git stash\n\n"
     git stash
 
-    echo "\n\nSee note A.3) git checkout gh-pages\n\n"
+    echo "\n\nSee note A.4) git checkout gh-pages\n\n"
+    git fetch
     git checkout gh-pages
+
+    echo "\n\nSee note A.5) git pull origin gh-pages\n\n"
     git pull origin gh-pages
 
-    echo "\n\nSee note A.4) cp -R ../gh-pages/html/* ../\n\n"
-    cp -R ../gh-pages/html/* ../
+    echo "\n\nSee note A.6)cp -R ../html/* ..\n\n"
+    cp -R ../html/* ..
 
-    echo "\n\nSee note A.5) git add -A\n\n"
+    echo "\n\nSee note A.7)rm -r ../html"
+    rm -r ../html
+
+    echo "\n\nSee note A.6) git add -A\n\n"
     git add -A
 
-    echo "\n\nSee note A.6) git commit -m '$COMMIT_MESSAGE'\n\n"
+    echo "\n\nSee note A.7) git commit -m '$COMMIT_MESSAGE'\n\n"
     git commit -m "$COMMIT_MESSAGE"
 
-    echo "\n\nSee note A.7) git push origin gh-pages\n\n"
+    echo "\n\nSee note A.8) git push origin gh-pages\n\n"
     git push origin gh-pages
 
-    echo "\n\nSee note A.8) git checkout master\n\n"
+    echo "\n\nSee note A.9) git checkout master\n\n"
     git checkout master
 
 else
