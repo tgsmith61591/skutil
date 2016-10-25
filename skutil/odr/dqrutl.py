83cb9573e116c2a18302b5c02d1ccaeb47e735ce
tgsmith61591 <tgsmith61591@gmail.com>
2016-10-25 05:04:29 -0500

    Removing 'absolute_import' from imports in dqrsl

--- skutil/odr/dqrutl.py
+++ skutil/odr/dqrutl.py
@@ -1,4 +1,4 @@
-from __future__ import print_function, division, absolute_import
+from __future__ import print_function, division #,absolute_import
 import numpy as np
 import dqrsl
 from sklearn.utils import check_array
