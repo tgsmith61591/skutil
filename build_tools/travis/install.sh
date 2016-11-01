1656a329b4288287b6869a72bbd280deb6c8d1ea
charlesdrotar <drotarcharles@yahoo.com>
2016-11-01 12:15:37 -0500

    add print statements with timing for debugging

--- build_tools/travis/install.sh
+++ build_tools/travis/install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+echo -e "\nA : `date` \n"
 case "${TRAVIS_PYTHON_VERSION}" in
     "2.7")
         wget https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh -O $HOME/miniconda.sh;
@@ -9,24 +10,33 @@ case "${TRAVIS_PYTHON_VERSION}" in
         ;;
 esac
 
+echo -e "\nB : `date` \n"
 bash $HOME/miniconda.sh -b -p $HOME/miniconda
+echo -e "\nC : `date` \n"
 export PATH="$HOME/miniconda/bin:$PATH"
+echo -e "\nD : `date` \n"
 hash -r
+echo -e "\nE : `date` \n"
 conda config --set always_yes yes --set changeps1 no
+echo -e "\nF : `date` \n"
 conda update -q conda
 
 # Useful for debugging any issues with conda
+echo -e "\nG : `date` \n"
 conda info -a
 
 # Dependencies
+echo -e "\nH : `date` \n"
 conda create -q -n test-environment python=$TRAVIS_PYTHON_VERSION numpy scipy pandas coverage cython
+echo -e "\nI : `date` \n"
 source activate test-environment
 
 # Coverage packages
+echo -e "\nJ : `date` \n"
 if [[ "$TRAVIS_PYTHON_VERSION" == "2.7" ]]; then
       conda install --yes -c dan_blanchard python-coveralls nose-cov;
 fi
-
+echo -e "\nK : `date` \n"
 case "${COMBINATION}" in
     "sklearn-0-17-matplotlib-seaborn")
         pip install scikit-learn==0.17.1
@@ -45,7 +55,9 @@ case "${COMBINATION}" in
         pip install scikit-learn==0.18
         ;;
 esac
-
+echo -e "\nL : `date` \n"
 pip install coveralls
+echo -e "\nM : `date` \n"
 pip install http://h2o-release.s3.amazonaws.com/h2o/rel-turchin/9/Python/h2o-3.8.2.9-py2.py3-none-any.whl
+echo -e "\nN : `date` \n"
 python setup.py develop
