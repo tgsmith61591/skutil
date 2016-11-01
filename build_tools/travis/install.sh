a7c7ae85a601a03155ef56a18c80d226a490d2bc
charlesdrotar <drotarcharles@yahoo.com>
2016-11-01 11:28:56 -0500

    extract installation into install.sh.

--- build_tools/travis/install.sh
+++ build_tools/travis/install.sh
@@ -1,5 +1,32 @@
 #!/bin/bash
 
+case "${TRAVIS_PYTHON_VERSION}" in
+    "2.7")
+        wget https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh -O miniconda.sh;
+        ;;
+    "3.5")
+        wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
+        ;;
+esac
+
+miniconda.sh -b -p $HOME/miniconda
+export PATH="$HOME/miniconda/bin:$PATH"
+hash -r
+conda config --set always_yes yes --set changeps1 no
+conda update -q conda
+
+# Useful for debugging any issues with conda
+conda info -a
+
+# Dependencies
+conda create -q -n test-environment python=$TRAVIS_PYTHON_VERSION numpy scipy pandas coverage cython
+source activate test-environment
+
+# Coverage packages
+if [[ "$TRAVIS_PYTHON_VERSION" == "2.7" ]]; then
+      conda install --yes -c dan_blanchard python-coveralls nose-cov;
+fi
+
 case "${COMBINATION}" in
     "sklearn-0-17-matplotlib-seaborn")
         pip install scikit-learn==0.17.1
@@ -18,3 +45,7 @@ case "${COMBINATION}" in
         pip install scikit-learn==0.18
         ;;
 esac
+
+pip install coveralls
+pip install http://h2o-release.s3.amazonaws.com/h2o/rel-turchin/9/Python/h2o-3.8.2.9-py2.py3-none-any.whl
+python setup.py develop
